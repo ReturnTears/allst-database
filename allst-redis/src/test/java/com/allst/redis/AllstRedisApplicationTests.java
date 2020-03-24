36a25066bdb0d15c074bea2b98bd15985e2211b6
returntears <returntears@outlook.com>
2020-03-25 00:21:14 +0800

    MySQL 8.0 高级部分

--- allst-redis/src/test/java/com/allst/redis/AllstRedisApplicationTests.java
+++ allst-redis/src/test/java/com/allst/redis/AllstRedisApplicationTests.java
@@ -1,13 +1,22 @@
 package com.allst.redis;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
+/**
+ * redis常用功能测试类
+ */
 @SpringBootTest
 class AllstRedisApplicationTests {
 
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
     @Test
     void contextLoads() {
+        redisTemplate.opsForValue().set("addr", "重庆市渝北区");
     }
 
 }
