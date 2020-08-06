16500d7e9191ccb3e5011efa4eb072b922f1217c
JUNN <returntears@outlook.com>
2020-08-06 12:04:16 +0800

    redis redisson

--- allst-redis/src/main/java/com/allst/redis/controller/IndexController.java
+++ allst-redis/src/main/java/com/allst/redis/controller/IndexController.java
@@ -7,6 +7,7 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
@@ -29,14 +30,11 @@ public class IndexController {
         String clientID = UUID.randomUUID().toString();
         RLock rLock = redisson.getLock(lockKey);    // 获取锁的对象
         try {
-            Boolean result = redisTemplate.opsForValue().setIfAbsent(lockKey, clientID, 30, TimeUnit.SECONDS);
-            if (!result) {
-                return "Error";
-            }
+            redisTemplate.opsForValue().setIfAbsent(lockKey, clientID, 30, TimeUnit.SECONDS);
 
             rLock.lock(30, TimeUnit.SECONDS);// 在执行32行代码时, 会在这行代码开启一个分线程执行续命逻辑
 
-            int stock = Integer.parseInt(redisTemplate.opsForValue().get("stock"));     // jedis.get("stock")
+            int stock = Integer.parseInt(Objects.requireNonNull(redisTemplate.opsForValue().get("stock")));     // jedis.get("stock")
             if (stock > 0) {
                 int realStock = stock - 1;
                 redisTemplate.opsForValue().set("stock", realStock + "");   //  jedis.set("stock")
@@ -45,9 +43,9 @@ public class IndexController {
                 System.out.println("扣减失败, 库存不足");
             }
         } finally {
-            /*if (clientID.equals(redisTemplate.opsForValue().get(lockKey))) {
+            if (clientID.equals(redisTemplate.opsForValue().get(lockKey))) {
                 redisTemplate.delete(lockKey);
-            }*/
+            }
             rLock.unlock();     // 释放锁
         }
         return "Success";
