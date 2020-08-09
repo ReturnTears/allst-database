8beca8555cc1280a1c684f7caf71ad196866a16f
returntears <returntears@outlook.com>
2020-08-09 19:01:15 +0800

    jedis String测试

--- allst-redis/src/test/java/com/allst/redis/AllstRedisApplicationTests.java
+++ allst-redis/src/test/java/com/allst/redis/AllstRedisApplicationTests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import redis.clients.jedis.Jedis;
 
 import java.util.Set;
 
@@ -74,5 +75,38 @@ class AllstRedisApplicationTests {
     @Test
     void redisDelLoads() {
         redisUtil.del("k2", "k3");
+    }
+
+    /**
+     * jedis测试
+     */
+    @Test
+    void jedisLoads() {
+        Jedis jedis = new Jedis("127.0.0.1", 6379);
+        System.out.println(jedis.ping());
+        jedis.set("user:name", "xiaohu");
+        System.out.println(jedis.get("user:name"));
+    }
+
+    /**
+     * jedis基本命令操作
+     */
+    @Test
+    void jedisBasicLoads() {
+        Jedis jedis = new Jedis("127.0.0.1", 6379);
+        System.out.println("清空数据:" + jedis.flushDB());
+        System.out.println("判断某个键是否存在:" + jedis.exists("user"));
+        System.out.println("新增:" + jedis.set("user:name", "XiaoMing"));
+        System.out.println("新增:" + jedis.set("user:age", "18"));
+        System.out.println("新增:" + jedis.set("user:sex", "18"));
+        Set<String> keys = jedis.keys("*");
+        System.out.println(keys);
+        System.out.println("删除键:" + jedis.del("user:sex"));
+        System.out.println("查看键类型:" + jedis.type("user:age"));
+        System.out.println("随机返回一个key: " + jedis.randomKey());
+        System.out.println("重名key: " + jedis.rename("user:name", "user:dename"));
+        System.out.println("索引查询: " + jedis.select(0));
+        System.out.println("key数量: " + jedis.dbSize());
+        //System.out.println("清库：" + jedis.flushAll());
     }
 }
