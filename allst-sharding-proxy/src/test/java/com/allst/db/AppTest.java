95a08b422b9638a9c640733a505f79606f8595e2
returntears <returntears@outlook.com>
2021-09-06 01:29:36 +0800

    sharding-proxy:测试新增

--- allst-sharding-proxy/src/test/java/com/allst/db/AppTest.java
+++ allst-sharding-proxy/src/test/java/com/allst/db/AppTest.java
@@ -25,4 +25,18 @@ public class AppTest {
         List<Position> list = positionRepository.findAll();
         list.forEach(System.out::println);
     }
+
+    /**
+     * sharding-proxy 测试新增
+     * 报错： Caused by: java.sql.SQLException: 2Unknown exception: [no table route info]
+     * MySQL-CLI 执行新增也会报错
+     */
+    @Test
+    public void test2() {
+        Position position = new Position();
+        position.setName("tiger");
+        position.setSalary("30000");
+        position.setCity("shanghai");
+        positionRepository.save(position);
+    }
 }
