4a791713ba4aece5d7b714f1ac2fe8ac7cb9c319
returntears <returntears@outlook.com>
2021-10-24 21:04:31 +0800

    Neo4j

---
+++ allst-neo4j/src/main/java/com/allst/db/AllstNeo4jApplication.java
@@ -0,0 +1,13 @@
+package com.allst.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllstNeo4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllstNeo4jApplication.class, args);
+	}
+
+}
