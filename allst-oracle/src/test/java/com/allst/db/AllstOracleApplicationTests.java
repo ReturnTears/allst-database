fc64b7af62dadfafeef67bc06a509413f003d2e8
returntears <returntears@outlook.com>
2022-09-19 20:25:45 +0800

    test

--- allst-oracle/src/test/java/com/allst/db/AllstOracleApplicationTests.java
+++ allst-oracle/src/test/java/com/allst/db/AllstOracleApplicationTests.java
@@ -3,11 +3,25 @@ package com.allst.db;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @SpringBootTest
 class AllstOracleApplicationTests {
 
     @Test
     void contextLoads() {
+        List<String> list1 = List.of("1", "2", "3");
+        List<String> list2 = List.of();
+        List<String> result = list1.stream().filter(i1 -> !list2.contains(i1)).collect(Collectors.toList());
+
+        System.out.println(result);
+
+        List<String> list3 = List.of();
+        List<String> list4 = List.of("1", "2", "3");
+        List<String> result2 = list3.stream().filter(i1 -> !list4.contains(i1)).collect(Collectors.toList());
+
+        System.out.println(result2);
     }
 
 }
