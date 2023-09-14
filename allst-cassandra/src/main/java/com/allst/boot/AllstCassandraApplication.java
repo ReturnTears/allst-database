f473b32a44984042da6041ef09877b778a8b5145
returntears <returntears@outlook.com>
2023-09-14 22:25:45 +0800

    Cassandra

---
+++ allst-cassandra/src/main/java/com/allst/boot/AllstCassandraApplication.java
@@ -0,0 +1,11 @@
+package com.allst.boot;
+
+/**
+ * @author Hutu
+ * @since 2023-09-14 下午 10:16
+ */
+public class AllstCassandraApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
