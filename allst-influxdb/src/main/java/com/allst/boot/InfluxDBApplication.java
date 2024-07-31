479e18223ca4f81f53c26c772c001889df76cf8a
returntears <returntears@outlook.com>
2024-07-31 22:09:04 +0800

    Start InfluxDB study trip

---
+++ allst-influxdb/src/main/java/com/allst/boot/InfluxDBApplication.java
@@ -0,0 +1,11 @@
+package com.allst.boot;
+
+/**
+ * @author Hutu
+ * @since 2024-07-31 下午 09:56
+ */
+public class InfluxDBApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
