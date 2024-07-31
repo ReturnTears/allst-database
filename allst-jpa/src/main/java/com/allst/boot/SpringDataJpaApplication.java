abb8c5032ffac0e8d734d4048019c919497ec9b7
returntears <returntears@outlook.com>
2024-07-31 22:17:51 +0800

    Start Spring Data Jpa study trip

---
+++ allst-jpa/src/main/java/com/allst/boot/SpringDataJpaApplication.java
@@ -0,0 +1,11 @@
+package com.allst.boot;
+
+/**
+ * @author Hutu
+ * @since 2024-07-31 下午 10:09
+ */
+public class SpringDataJpaApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
